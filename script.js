4b95ba3323dd741289b41460a061f0d8e3d781ae
rajeshprema <122607002+rajeshprema@users.noreply.github.com>
2023-08-22 12:13:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var something = setTimeout(() => {
+    console.log('one')
+    
+   var something = setTimeout(() => {
+      console.log('two')
+    
+    var something = setTimeout(() => {
+        console.log('three')
+      
+ var something = setTimeout(() => {
+    console.log('fore')
+  
+
+  var something = setTimeout(() => {
+    console.log('five')
+
+  var something = setTimeout(() => {
+      console.log('six')
+    
+    var something = setTimeout(() => {
+        console.log('savan')
+    
+      var something = setTimeout(() => {
+          console.log('eight')
+      
+       var something = setTimeout(() => {
+          console.log('nine')
+      
+           something = setTimeout(() => {
+           console.log('ten happy independence day');
+           }, 1000) 
+           },1000)
+          },1000)
+      }, 1000)
+    }, 1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  
+
+
+
+
+
+
+
+        
